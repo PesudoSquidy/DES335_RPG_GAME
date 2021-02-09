ef52013fd33006fd35c6f824279ba2961736b3ae
PesudoSquidy <PesudoSquidy@.github.com>
2021-02-09 11:24:29 +0800

    DES335 Unity Initial Proejct Push

---
+++ DES335_RPG_GAME/Assets/Self_Scripts/Destructibles.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destructibles : MonoBehaviour
+{
+
+    public int health;
+
+    void OnTriggerEnter(Collider collider)
+    {
+        if (collider.gameObject.tag == "Weapon")
+        {
+            --health;
+
+            if (health <= 0)
+                DropLoot();
+        }
+
+
+        Debug.Log(collider.gameObject.name);
+        Debug.Log(collider.gameObject.tag);
+    }
+
+    void DropLoot()
+    {
+        Debug.Log("Drop LOOT!");
+
+        //Hide game object
+        gameObject.SetActive(false);    
+    }
+}
