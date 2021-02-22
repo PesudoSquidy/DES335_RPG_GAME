b3a9edf8406258eb79b692ba692578044b88d170
PesudoSquidy <PesudoSquidy@.github.com>
2021-02-22 16:25:08 +0800

    2 modification
    - Added UI icons, buttons, misc etc
    - Remove code on saving EditorGUI Layout (commented out) - SetupInputLayer.cs

--- DES335_RPG_GAME/Assets/DES335_RPG/Scripts/ItemPickup.cs
+++ DES335_RPG_GAME/Assets/DES335_RPG/Scripts/ItemPickup.cs
@@ -6,9 +6,7 @@ public class ItemPickup : MonoBehaviour
 {
     [SerializeField] private Item item;
 
-
     private GameObject player;
-
 
     // Start is called before the first frame update
     void Start()
