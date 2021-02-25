0e44bec71ffad64c1c41f863d30f45e9f751969b
PesudoSquidy <PesudoSquidy@.github.com>
2021-02-25 15:50:42 +0800

    Cleanup on asset

--- DES335_RPG_GAME/Assets/DES335_RPG/Scripts/PlayerMovement.cs
+++ DES335_RPG_GAME/Assets/DES335_RPG/Scripts/PlayerMovement.cs
@@ -4,21 +4,21 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    private Camera cam;
 
-    private Transform trans;
+
+    [SerializeField]
+    private float moveSpeed = 5;
+
     private Rigidbody2D rb;
-    public float moveSpeed;
-
     private Animator anim;
 
-    private Camera cam;
+    Vector2 movement;
+
 
     // Start is called before the first frame update
     void Start()
     {
-        if (gameObject.GetComponent<Transform>() != null)
-            trans = gameObject.GetComponent<Transform>();
-
         if (anim == null)
             anim = gameObject.GetComponent<Animator>();
 
@@ -32,47 +32,23 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        anim.SetFloat("horizontalSpeed", Mathf.Abs(Input.GetAxisRaw("Horizontal")));
-        anim.SetFloat("verticalSpeed", Input.GetAxisRaw("Vertical"));
-        
+
         // Calculating Mouse position in world space with respect to the player's pos from Camera
         Vector3 mousePosition = cam.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10));
         Vector2 direction = new Vector2(mousePosition.x - transform.position.x, mousePosition.y - transform.position.y);
 
-        if (Input.GetButton("Horizontal"))
-        {
-            // Face Right
-            //if(Input.GetAxisRaw("Horizontal") > 0)
-            if (direction.x > 0)
-                trans.localScale = new Vector3(10, 10, 1);
+        // Input
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
 
-            // Face Left
-            else if (direction.x < 0)
-            //else if(Input.GetAxisRaw("Horizontal") < 0)
-            {
-                trans.localScale = new Vector3(-10, 10, 1);
-                
-            }
-            trans.position += Vector3.right * moveSpeed * Time.deltaTime * Input.GetAxisRaw("Horizontal");
-        }
+        anim.SetFloat("horizontalSpeed", movement.x);
+        anim.SetFloat("verticalSpeed", movement.y);
+        anim.SetFloat("speed", movement.sqrMagnitude);
+    }
 
-        if(Input.GetButton("Vertical"))
-        {
-            trans.position += Vector3.up * moveSpeed * Time.deltaTime * Input.GetAxisRaw("Vertical");
-        }
-
-        if(Input.GetButtonDown("Horizontal"))
-        {
-            // Set direction facing to use the mose relevant animation
-            anim.SetBool("faceHorizontal", true);
-            anim.SetBool("faceVertical", false);
-        }
-        else if(Input.GetButtonDown("Vertical"))
-        {
-
-            // Set direction facing to use the mose relevant animation
-            anim.SetBool("faceHorizontal", false);
-            anim.SetBool("faceVertical", true);
-        }
+    void FixedUpdate()
+    {
+        // Movement
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
     }
 }
