714e46eef3d1c08ddb4def3c368ea31d5dd216f4
PesudoSquidy <PesudoSquidy@.github.com>
2021-02-27 20:42:25 +0800

    New features & changes
    Changes
    - Eagle drop different item
    - Treant drop different item
    
    Features - New item
    - Coin
    - Gem
    - Cherry

--- DES335_RPG_GAME/Assets/DES335_RPG/Scripts/LootDrop.cs
+++ DES335_RPG_GAME/Assets/DES335_RPG/Scripts/LootDrop.cs
@@ -7,8 +7,8 @@ public class LootDrop : MonoBehaviour
     [SerializeField]
     private List<GameObject> lootDrop;
 
-    [SerializeField] [Range(0.0f, 10.0f)]
-    private int dropRate;
+    //[SerializeField] [Range(0.0f, 10.0f)]
+    //private int dropRate;
 
     [SerializeField] [Range(0, 10)]
     private int dropAmount;
@@ -21,16 +21,17 @@ public class LootDrop : MonoBehaviour
         {
             for(int i = 0; i < dropAmount; ++i)
             {
-                int randNo = Random.Range(0, dropRate);
-                float randPosX = Random.Range(0.0f, 0.5f) + gameObject.transform.position.x;
-                float randPosY = Random.Range(0.0f, 0.5f) + gameObject.transform.position.y;
-
-                Vector3 randPos = new Vector3(randPosX, randPosY, gameObject.transform.position.z);
+                int randNo = Random.Range(0, lootDrop.Count);
+                //float randPosX = Random.Range(0.0f, 0.5f) + gameObject.transform.position.x;
+                //float randPosY = Random.Range(0.0f, 0.5f) + gameObject.transform.position.y;
+                //
+                //Vector3 randPos = new Vector3(randPosX, randPosY, gameObject.transform.position.z);
 
                 if(randNo > lootDrop.Count)
                     randNo %= lootDrop.Count;
 
-                Instantiate(lootDrop[randNo], randPos, gameObject.transform.rotation);
+                //Instantiate(lootDrop[randNo], randPos, gameObject.transform.rotation);
+                Instantiate(lootDrop[randNo], gameObject.transform.position, gameObject.transform.rotation);
             }
         }
     }
