8d2fdfb889d31358ed7bea04334e85eb9869bd80
DrWodahs <yeeyilin@hotmail.com>
2021-03-02 16:37:37 +0800

    Create Tunnel Exits
    - Block exits when combat

--- DES335_RPG_GAME/Assets/DES335_RPG/Scripts/Enemy/CombatRoomEnemySpawnManager.cs
+++ DES335_RPG_GAME/Assets/DES335_RPG/Scripts/Enemy/CombatRoomEnemySpawnManager.cs
@@ -9,18 +9,34 @@ public class CombatRoomEnemySpawnManager : MonoBehaviour
 
     [SerializeField] private GameObject[] spawnPoints;
 
+    [SerializeField] private GameObject LockRoom;
+
+    GameObject[] enemies;
+
     // Start is called before the first frame update
     void Start()
     {
         if (enemyPrefabs.Length > 0)
         {
+            enemies = new GameObject[enemyAmount];
             for (int i = 0; i < enemyAmount; ++i)
             {
                 int randNo = Random.Range(0, enemyPrefabs.Length);
                 int randSP = Random.Range(0, spawnPoints.Length);
 
-                Instantiate(enemyPrefabs[randNo], spawnPoints[randSP].transform.position, Quaternion.identity);
+                enemies[i] = Instantiate(enemyPrefabs[randNo], spawnPoints[randSP].transform.position, Quaternion.identity);
             }
         }
+    }
+
+    void Update()
+    {
+        for (int i = 0; i < enemyAmount; ++i)
+        {
+            if (enemies[i] != null)
+                return;
+        }
+
+        LockRoom.SetActive(false);
     }
 }
